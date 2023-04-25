9c9127def048cb1313f97aa7e4cc3d34c7d203c3
Alex Shein <aleksshein4dev@gmail.com>
2023-04-25 20:48:44 +0200

    update recipe-edit routing

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+  styleUrls: ['./recipe-edit.component.css'],
 })
-export class RecipeEditComponent {
+export class RecipeEditComponent implements OnInit {
+  recipeIndex: number;
+  isEditMode = false;
+  recipe: Recipe;
+  constructor(
+    private route: ActivatedRoute,
+    private recipesService: RecipeService
+  ) {}
 
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      if (params['index'] != null) {
+        this.recipeIndex = +params['index'];
+        this.isEditMode = true;
+      }
+      this.getRecipe();
+    });
+  }
+
+  getRecipe() {
+    if (this.isEditMode) {
+      this.recipe = this.recipesService.getRecipe(this.recipeIndex);
+    } else {
+      this.recipe = new Recipe('', '', '', []);
+    }
+  }
 }

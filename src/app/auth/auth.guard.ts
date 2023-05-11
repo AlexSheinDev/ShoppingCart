2e4b00f32cfafd7657e6b2f9a7ea18020d49a9e6
Alex Shein <aleksshein4dev@gmail.com>
2023-05-11 11:17:12 +0200

    update angular to 16v

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,23 +1,14 @@
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanActivate {
+export class AuthGuard {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
+  canActivate():
     | Observable<boolean | UrlTree>
     | Promise<boolean | UrlTree>
     | boolean
--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -1,25 +1,17 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  Resolve,
-  RouterStateSnapshot,
-} from '@angular/router';
 import { Recipe } from './recipe.model';
 import { Observable } from 'rxjs';
 import { DataStorageService } from '../shared/data-storage.service';
 import { RecipeService } from './recipe.service';
 
 @Injectable({ providedIn: 'root' })
-export class RecipeResolverService implements Resolve<Recipe[]> {
+export class RecipeResolverService {
   constructor(
     private dataStorageService: DataStorageService,
     private recipeService: RecipeService
   ) {}
 
-  resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<Recipe[]> | Promise<Recipe[]> | Recipe[] {
+  resolve(): Observable<Recipe[]> | Promise<Recipe[]> | Recipe[] {
     const recipes = this.recipeService.getRecipes();
 
     if (recipes.length === 0) {
